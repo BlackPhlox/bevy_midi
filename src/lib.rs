15fb9fca10110eaaf7a6c6ee09b43f1e0db29098
grace125 <74109712+grace125@users.noreply.github.com>
2022-08-07 11:59:12 -0700

    Add flats to KEY_RANGE constant
    Co-authored-by: Mikkel Rasmussen <theluja@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@ pub mod input;
 pub mod output;
 
 pub const KEY_RANGE: [&str; 12] = [
-    "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B",
+    "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B",
 ];
