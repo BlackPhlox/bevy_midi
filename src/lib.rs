80eeb7f281ac8e1eea58f4156531e5f57453c131
Grace Schorno <graceschorno@protonmail.com>
2022-08-08 16:31:07 -0700

    Add channel method to MidiMessage

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@ pub const KEY_RANGE: [&str; 12] = [
     "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B",
 ];
 
-const NOTE_ON_SIGNATURE: u8 = 0b10010000;
-const NOTE_OFF_SIGNATURE: u8 = 0b10000000;
+const NOTE_ON_STATUS: u8 = 0b10010000;
+const NOTE_OFF_STATUS: u8 = 0b10000000;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub struct MidiMessage {
@@ -21,10 +21,15 @@ impl From<[u8; 3]> for MidiMessage {
 
 impl MidiMessage {
     pub fn is_note_on(&self) -> bool {
-        (self.msg[0] & 0b11110000) == NOTE_ON_SIGNATURE
+        (self.msg[0] & 0b11110000) == NOTE_ON_STATUS
     }
 
     pub fn is_note_off(&self) -> bool {
-        (self.msg[0] & 0b11110000) == NOTE_OFF_SIGNATURE
+        (self.msg[0] & 0b11110000) == NOTE_OFF_STATUS
+    }
+
+    /// Get the channel of a message, assuming the message is not a system message.
+    pub fn channel(&self) -> u8 {
+        self.msg[0] & 0b00001111
     }
 }
